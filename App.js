c16a58a09ba98baa1cd99d29d15c001b3ba5d921
andangrd <ezh2707@gmail.com>
2019-01-29 00:39:32 +0700

    update to version 1.4.2

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Linking
 } from 'react-native';
 import Markdown from 'react-native-markdown-package';
 
@@ -32,9 +33,9 @@ const text = `
 4. List4.
 
 
-You can also put some url as a link [like This](https://this.isurl.com) or write it as a plain text:
-  https://this.isurl.com
-  <thisis@email.com>
+You can also put some url as a link [like This](https://www.google.com) or write it as a plain text:
+  https://www.google.com
+  <mailme@gmail.com>
 
 ---
 
@@ -72,7 +73,10 @@ export default class App extends Component<{}> {
           <Text style={styles.welcome}>
             Welcome to React Native!
           </Text>
-          <Markdown styles={styles.markdown} >
+          <Markdown 
+            styles={styles.markdown} 
+            onLink={(url) => Linking.openURL(url)}
+          >
             { text } 
           </Markdown>
         
